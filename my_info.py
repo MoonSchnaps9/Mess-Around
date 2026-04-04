1c3b23be478133dee6ffa549f2ec8fd2cd37947b
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-04 16:21:09 +0200

    Remove 2 lines

--- my_info.py
+++ my_info.py
@@ -1,5 +1,3 @@
 print("Name: Guillaume")
 print("Job: Continuous Improvement Expert")
 print("Why I want to learn Python: I want to work in Space industry, not sure if Python is used but I want to be able to use it if it is")
-
-
