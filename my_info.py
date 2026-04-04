b02a47794d1c73f4bea6c3ca74babbe66cc1167c
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-04 16:55:47 +0200

    Test

--- my_info.py
+++ my_info.py
@@ -1,3 +1,4 @@
 print("Name: Guillaume")
 print("Job: Continuous Improvement Expert")
 print("Why I want to learn Python: I want to work in Space industry, not sure if Python is used but I want to be able to use it if it is")
+Test
