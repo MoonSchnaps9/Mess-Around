7a80feec61c08879cde23ca62507662841365fe4
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-18 16:18:48 +0200

    2nd attempt (Data type correct but result wrong)

--- 100 days of code: Python Bootcamp/Day 02/Project Tip Calculator.py
+++ 100 days of code: Python Bootcamp/Day 02/Project Tip Calculator.py
@@ -4,5 +4,6 @@ total_bill = input("What was the total bill? $ ")
 tip_user_want_to_give = input("How much tip would you like to give? 10, 12, or 15? ")
 Number_of_People_to_Split_Bill = input("How many people to split the bill? ")
 
-Final_price_per_person = (round,2((total_bill * int(tip_user_want_to_give / 100) ) / int(Number_of_People_to_Split_Bill)))
+Final_price_per_person = (int(total_bill) * (int(tip_user_want_to_give) / 100)) / int(Number_of_People_to_Split_Bill)
+round(Final_price_per_person, 2)
 print(f"Each person should pay: ${Final_price_per_person}")
