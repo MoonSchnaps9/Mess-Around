8f24d664c21a86f053486b6c8d38958c610a2322
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-18 14:56:15 +0200

    Resolution (2 methods)

--- 100 days of code: Python Bootcamp/Day 02/Debugging Exercise to correct Data type.py
+++ 100 days of code: Python Bootcamp/Day 02/Debugging Exercise to correct Data type.py
@@ -1,1 +1,8 @@
-print("number of letters in your name: " + len(input("Enter your name")))
+#Method 1
+print("number of letters in your name: " + str(len(input("Enter your name"))))
+
+#Method 2
+name_of_user = input("Enter your name ")
+length_of_name = len(name_of_user)
+
+print("number of letters in your name: " + str(length_of_name))
