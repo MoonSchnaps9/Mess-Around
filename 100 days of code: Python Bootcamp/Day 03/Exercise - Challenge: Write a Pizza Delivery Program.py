680fd64b7825ca223c363714011e2cf333e7d2a6
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-21 21:14:23 +0200

    Exercise - Challenge: Create Pizza Delivery Program

---
+++ 100 days of code: Python Bootcamp/Day 03/Exercise - Challenge: Write a Pizza Delivery Program.py
@@ -0,0 +1,16 @@
+# Build an automatic pizza order program that uses input() to get a user's preferences and calculate their final bill.
+# Pricing:
+
+# Small (S): $15
+# Medium (M): $20
+# Large (L): $25
+# Pepperoni on small: +$2
+# Pepperoni on medium or large: +$3
+# Extra cheese (any size): +$1
+
+# Expected output example:
+# Welcome to Python Pizza Deliveries!
+# What size pizza do you want? S, M or L: L
+# Do you want pepperoni on your pizza? Y or N: Y
+# Do you want extra cheese? Y or N: N
+# Your final bill is: $28.
