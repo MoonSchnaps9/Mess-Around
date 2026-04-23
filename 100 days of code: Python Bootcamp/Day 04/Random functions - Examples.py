70b9e826b7fa1437b4f01a16dfe78317a0d77999
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-23 20:21:26 +0200

    Examples of Random functions

---
+++ 100 days of code: Python Bootcamp/Day 04/Random functions - Examples.py
@@ -0,0 +1,13 @@
+import random
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+random_number_0_to_1 = random.random()
+print(random_number_0_to_1)
+
+random_number_0_to_1_bis = random.random() * 15
+print(random_number_0_to_1_bis)
+
+random_float = random.uniform(1,10)
+print(random_float)
