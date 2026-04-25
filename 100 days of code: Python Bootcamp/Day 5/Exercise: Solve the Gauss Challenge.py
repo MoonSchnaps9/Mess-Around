5ab5ae8aef6219202c12a3c6d1e64de3407a3df8
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-25 13:40:39 +0200

    Solved!

--- 100 days of code: Python Bootcamp/Day 5/Exercise: Solve the Gauss Challenge.py
+++ 100 days of code: Python Bootcamp/Day 5/Exercise: Solve the Gauss Challenge.py
@@ -1,2 +1,9 @@
 #Gauss Challenge
 #Work out the total of the number between 1 and 100, inclusive of both 1 and 100
+
+total = 0
+for number in range (1, 101, 1):
+#I know ",1" is redundant, but this is for me to understand the range()
+    total += number
+
+print(total)
