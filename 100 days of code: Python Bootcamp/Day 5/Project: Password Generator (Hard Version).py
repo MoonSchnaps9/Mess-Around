162633941b62d171bd2a8aac6aae577e51becebb
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-25 16:26:25 +0200

    A bit disappointed as I asked Claude for hints (I struggled even more here) but it works.

--- 100 days of code: Python Bootcamp/Day 5/Project: Password Generator (Hard Version).py
+++ 100 days of code: Python Bootcamp/Day 5/Project: Password Generator (Hard Version).py
@@ -7,7 +7,24 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 # the final password does not follow a pattern.
 #Every time you generate a password, the position of the symbols, numbers, and letters are different.
 
+
+#Trying to add random.shuffle to the easy version and work with one list only
+import random
+
 print("Welcome to the PyPassword Generator!")
 user_number_letters = int(input("How many letters would you like in your password?\n"))
 user_number_symbols = int(input("how many symbols would you like?\n"))
 user_number_number = int(input("How many numbers would you like?\n"))
+
+password_result = []
+
+for letters1 in range(user_number_letters):
+    password_result += random.choice(letters)
+for symbols2 in range(user_number_symbols):
+    password_result += random.choice(symbols)
+for numbers3 in range(user_number_number):
+    password_result += random.choice(numbers)
+
+random.shuffle(password_result)
+Final_password = "".join(password_result)
+print("Your password is:" +" "+ Final_password)
