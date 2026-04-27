9b8c3fceefb4fcdb4cbd1b3021761420ae6eff7c
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-04-28 00:42:23 +0200

    ClaudeAI Exercise: Lottery

---
+++ 100 days of code: Python Bootcamp/Day 05 bis (Claude Exercises)/ClaudeAI Exercise: Lottery.py
@@ -0,0 +1,14 @@
+# Exercise: Lottery Ticket Generator
+
+lottery_pool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# Write a program that:
+
+# Asks the user how many lottery tickets they want to generate
+# Each ticket has 5 unique numbers picked from the pool
+# Numbers on each ticket are sorted in ascending order
+# Prints each ticket numbered
+
+# Example output:
+# Ticket 1: [3, 7, 11, 15, 18]
+# Ticket 2: [1, 6, 9, 14, 20]
