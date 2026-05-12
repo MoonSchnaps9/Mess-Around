340f6ae4db5a3fef4937a4ebec8bf67f0c3be31d
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-12 20:27:32 +0200

    Step 1 of the hangman game challenge

---
+++ 100 days of code: Python Bootcamp/Day 07/Step 1 - Hangman game.py
@@ -0,0 +1,3 @@
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it
+#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase
+#Check if the letter the user guessed (guess) is on of the letters in the chosen_word. Print "Right" if it is, "Wrong" if it's not
