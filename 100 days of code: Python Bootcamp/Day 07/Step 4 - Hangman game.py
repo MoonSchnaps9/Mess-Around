14af7176f6f1adcd00a3f548f1087762bacb6c4a
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-14 23:41:03 +0200

    Solved (Quite fast here)

--- 100 days of code: Python Bootcamp/Day 07/Step 4 - Hangman game.py
+++ 100 days of code: Python Bootcamp/Day 07/Step 4 - Hangman game.py
@@ -2,18 +2,68 @@
 
 #Part 1
 # Create a variable called lives to keep track of the number of lives left. 
-#Set lives to equal 6
+# Set lives to equal 6
 
 #Part 2
-#If guess is not a letter in the chosen_word, then reduce lives by 1
-#If lives goes down to 0 then the game should end, and it should print " You loose "
+# If guess is not a letter in the chosen_word, then reduce lives by 1
+# If lives goes down to 0 then the game should end, and it should print " You loose "
 
-#Part 3 
+# Part 3 
 # print the ASCII art from the list stages that corresponds to the current number of lives the users has remaining
 
 #------------------------------------------------------------------------------------------------------------------------------------------------
-
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''']
 
 word_list = ["earth", "jupiter", "andromeda", "nebula", "moon", "asteroid", "galaxy"]
 
@@ -29,10 +79,17 @@ print(placeholder)
 
 display = placeholder
 correct_guess = []
+lives = 6
+game_on = True
 
-while "_" in display:
+while game_on:
     new_display = ""
     guess = input("Guess a letter:\n").lower()
+
+    if  guess not in chosen_word:
+        lives -= 1
+        print(f"Wrong, you've lost 1 life. Remaining: {lives}")
+
     for letter in chosen_word:
         if letter == guess:
                 correct_guess.append(letter)
@@ -44,4 +101,11 @@ while "_" in display:
     display = new_display
     
     print(display)
-print("You win!")
+    print(f"{stages[lives]}")
+
+    if "_" not in display:
+         print("Congrats, you won!")
+         game_on = False
+    elif lives == 0:
+         print("Sorry, you lost!")
+         game_on = False
