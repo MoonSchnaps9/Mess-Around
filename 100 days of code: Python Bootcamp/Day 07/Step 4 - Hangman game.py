9240d347d879c0ec33aedc8dfb9583f02d1e0d7f
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-14 22:33:44 +0200

    Instructions for Step 4

---
+++ 100 days of code: Python Bootcamp/Day 07/Step 4 - Hangman game.py
@@ -0,0 +1,47 @@
+# "INSTRUCTIONS of Step 4"
+
+#Part 1
+# Create a variable called lives to keep track of the number of lives left. 
+#Set lives to equal 6
+
+#Part 2
+#If guess is not a letter in the chosen_word, then reduce lives by 1
+#If lives goes down to 0 then the game should end, and it should print " You loose "
+
+#Part 3 
+# print the ASCII art from the list stages that corresponds to the current number of lives the users has remaining
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+import random
+
+word_list = ["earth", "jupiter", "andromeda", "nebula", "moon", "asteroid", "galaxy"]
+
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+placeholder = ""
+for letter in chosen_word:
+    placeholder += "_"+" "
+
+print(placeholder)
+
+display = placeholder
+correct_guess = []
+
+while "_" in display:
+    new_display = ""
+    guess = input("Guess a letter:\n").lower()
+    for letter in chosen_word:
+        if letter == guess:
+                correct_guess.append(letter)
+                new_display += letter+" "
+        elif letter in correct_guess:
+                new_display += letter+" "
+        else:
+            new_display += "_"+" "
+    display = new_display
+    
+    print(display)
+print("You win!")
