cd0fb417d9d3ef48a0654602d9ca42a070df40a6
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-16 16:08:46 +0200

    Exercise: Love Calculator Instructions

---
+++ 100 days of code: Python Bootcamp/Day 08/Exercise: Love Calculator.py
@@ -0,0 +1,7 @@
+# Love Calculator - Day 8 Exercise
+# Create a function called calculate_love_score(name1, name2)
+# 1. Count how many times each letter in "TRUE" appears in both names combined
+# 2. Count how many times each letter in "LOVE" appears in both names combined
+# 3. Combine the two totals to make a 2-digit number and print it
+# e.g. TRUE total = 5, LOVE total = 3 → Love Score = 53
+
