8c3ee90472d89d32defd316ce352e9f7554217b5
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-16 15:32:09 +0200

    Instructions

---
+++ 100 days of code: Python Bootcamp/Day 08/Exercise: Life in Weeks.py
@@ -0,0 +1,5 @@
+# Life in Weeks - Day 8 Exercise
+# Create a function called life_in_weeks(age)
+# Calculate how many weeks are left if we live until 90
+# Output format: "You have x weeks left."
+# (exact punctuation required — full stop at the end)
