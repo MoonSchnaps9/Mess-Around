ddeb32e07c752eb0113572133f39179ddc9b296e
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-16 16:56:43 +0200

    Solved

--- 100 days of code: Python Bootcamp/Day 08/Exercise: Love Calculator.py
+++ 100 days of code: Python Bootcamp/Day 08/Exercise: Love Calculator.py
@@ -5,3 +5,18 @@
 # 3. Combine the two totals to make a 2-digit number and print it
 # e.g. TRUE total = 5, LOVE total = 3 → Love Score = 53
 
+def calculate_love_score(name1, name2):
+    true_count = 0
+    love_count = 0
+    first_word = "TRUE"
+    second_word = "LOVE"
+    for letter in (name1 + name2).upper():
+        if letter in first_word:
+            true_count += 1
+        if letter in second_word:
+            love_count += 1
+    
+    love_score = str(true_count) + str(love_count)
+    print(love_score)
+
+calculate_love_score("Guillaume", "Luna")
