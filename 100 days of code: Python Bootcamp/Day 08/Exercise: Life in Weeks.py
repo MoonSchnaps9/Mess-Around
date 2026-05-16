539361987b62148d1742e4bec99eb1cd861b7052
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-16 15:43:28 +0200

    Resolution

--- 100 days of code: Python Bootcamp/Day 08/Exercise: Life in Weeks.py
+++ 100 days of code: Python Bootcamp/Day 08/Exercise: Life in Weeks.py
@@ -3,3 +3,11 @@
 # Calculate how many weeks are left if we live until 90
 # Output format: "You have x weeks left."
 # (exact punctuation required — full stop at the end)
+
+def life_in_weeks(age):
+    weeks_left = 4680-(age*52)
+    print(f"You have {weeks_left} weeks left.")
+
+life_in_weeks(35)
+
+#Or weeks_left = (90 - age) * 52
