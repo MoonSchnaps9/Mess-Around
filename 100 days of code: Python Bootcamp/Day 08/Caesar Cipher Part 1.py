bd00a61aab0631811fefab3f19bf05b15b147b74
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-16 18:28:45 +0200

    Instructions Caesar Cipher Part 1

---
+++ 100 days of code: Python Bootcamp/Day 08/Caesar Cipher Part 1.py
@@ -0,0 +1,18 @@
+#Angela (Teacher) created this below for us
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#End of Angela's help
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 1: Create a function called encrypt() that takes original_text 
+# and shift_amount as inputs
+
+# 2: Inside encrypt(), shift each letter forwards in the alphabet by shift_amount and print the encrypted text
+# Use index() to find the position of a letter in the alphabet list
+# e.g. plain_text = "hello", shift_amount = 1 → "ifmmp"
+# Output format: "Here is the encoded result: ifmmp"
