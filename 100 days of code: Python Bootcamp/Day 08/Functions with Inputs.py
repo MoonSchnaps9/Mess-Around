71a81ef0ccde85cc11a664093aeb3217d61a3f0b
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-16 15:08:44 +0200

    Resolution

--- 100 days of code: Python Bootcamp/Day 08/Functions with Inputs.py
+++ 100 days of code: Python Bootcamp/Day 08/Functions with Inputs.py
@@ -1,3 +1,12 @@
 # Create a fucntion called greet()
 # Write 3 print statements inside the fuction
 # call the greet() function and run your code
+
+
+def greet():
+    print("Welcome to the Earth's Solar System!")
+    print("This system is very well-reputated for only one thing:")
+    print("its amazing cookies! :D")
+
+
+greet()
