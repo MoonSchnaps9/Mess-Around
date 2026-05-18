c65c8f42d077a32c10652cf5e175e92a1d6df0c8
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-18 22:56:58 +0200

    Solution (I was a bit stuck with a variable problem when I moved them into the while loop though)

--- 100 days of code: Python Bootcamp/Day 08/Caesar Cipher part 3.py
+++ 100 days of code: Python Bootcamp/Day 08/Caesar Cipher part 3.py
@@ -1,12 +1,3 @@
-#Resources
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#---------------------------------------------------------------------------------------------------------------------------------------------------------
-
 #Part 3
 
 # 1: Import and print the logo from art.py at the start of the program
@@ -20,37 +11,60 @@ shift = int(input("Type the shift number:\n"))
 # If "no" → end the program
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
+#Import logo of the game
+from art import logo
+print(logo)
 
+#Alphabet list
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def decrypt(original_text=text, shift_amount=shift):
+#Decrypt function
+def decrypt(original_text, shift_amount):
     decrypted_message = ""
 
-    for letter in text:
-        position_in_alphabet_list = alphabet.index(letter)
-        position_in_alphabet_list_with_shift = position_in_alphabet_list - shift
-        final_position = position_in_alphabet_list_with_shift % 26
-        decrypted_message += alphabet[final_position]
+    for letter in original_text:
+        if letter not in alphabet:
+            decrypted_message += letter
+        else:
+            position_in_alphabet_list = alphabet.index(letter)
+            position_in_alphabet_list_with_shift = position_in_alphabet_list - shift_amount
+            final_position = position_in_alphabet_list_with_shift % 26
+            decrypted_message += alphabet[final_position]
     print(f"Here is the decoded result: {decrypted_message}")
 
-
-def encrypt(original_text=text, shift_amount=shift):
+#Encrypt function
+def encrypt(original_text, shift_amount):
     
     encrypted_message = ""
-    
-    for letter in text:
-        position_in_alphabet_list = alphabet.index(letter)
-        position_in_alphabet_list_with_shift = position_in_alphabet_list + shift
-        final_position = position_in_alphabet_list_with_shift % 26
-        encrypted_message += alphabet[final_position]
+    for letter in original_text:
+        if letter not in alphabet:
+            encrypted_message += letter
+        else:
+            position_in_alphabet_list = alphabet.index(letter)
+            position_in_alphabet_list_with_shift = position_in_alphabet_list + shift_amount
+            final_position = position_in_alphabet_list_with_shift % 26
+            encrypted_message += alphabet[final_position]
     print(f"Here is the encoded result: {encrypted_message}")
 
+#Main Function - Game menu
+def caesear():
+    game = True
+    while game:
+        user_choice = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
+        original_text = input("Type your message:\n").lower()
+        shift_amount = int(input("Type the shift number:\n"))
+        if user_choice == "encode":
+            encrypt(original_text, shift_amount)
+        elif user_choice == "decode":
+            decrypt(original_text, shift_amount)
+        else:
+            print(f"Try again and ensure to write 'encode' or 'decode' properly please :)")
 
-def caesear(user_choice = direction, original_text = text, shift_amount = shift):
-    if direction == "encode":
-        encrypt()
-    elif direction == "decode":
-        decrypt()
-    else:
-        print(f"Try again and ensure to write 'encode' or 'decode' properly please :)")
+        user_choice = input("Do you want to go again? (Yes or No)\n").lower()
+        if user_choice == "no":
+            print("Thank you and have a great day!")
+            game = False
 
+
+#Calling main function to run the game
 caesear()
