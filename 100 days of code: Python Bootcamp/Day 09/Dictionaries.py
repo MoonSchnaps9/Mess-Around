063cfbb29a5e227d77c71825d23348313c017e84
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-19 21:49:44 +0200

    Examples of dictionary usage

---
+++ 100 days of code: Python Bootcamp/Day 09/Dictionaries.py
@@ -0,0 +1,26 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "a piece of code that you can easily call over and over again.",
+    }
+
+print(programming_dictionary["Bug"])
+print(programming_dictionary)
+
+#Adding one more key
+programming_dictionary["Loop"] =  "The action of doing something over and over again."
+print(programming_dictionary)
+
+#Creating empty dictionary
+empty_dictionary = {}
+
+#Wipe an entire dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Edit an item in a dictionary
+programming_dictionary["Bug"] = "A celestial song that messes around with your program"
+print(programming_dictionary)
+
+#loop through a dictionary
+for key in programming_dictionary:
+    print(programming_dictionary[key])
