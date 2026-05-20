882ddcef1c0dd75cd8c4a0f06c787f4fb3bcd95a
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-20 23:16:40 +0200

    Instructions of Auction Program

---
+++ 100 days of code: Python Bootcamp/Day 09/Challenge: Auction Program.py
@@ -0,0 +1,14 @@
+# Blind Auction Project - Day 9
+# ─────────────────────────────
+# 1: Import and display the logo from art.py at the start of the program.
+# 2: Ask the user for their name 
+# 3: Ask the user for their bid amount
+# 4: Add the name (key) and bid (value) to the dictionary.
+# 5: Ask if there are other bidders.
+#         - If "yes": clear the screen and loop back
+#         - If "no": move on
+# 6: Find the highest bid in the dictionary.
+# 7: Print the winner's name and their winning bid amount.
+
+#---------------------------------------------------------------------------------------------
+
