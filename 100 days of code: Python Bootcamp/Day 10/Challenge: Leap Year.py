d80a4c450bfe2ec90298c884db79b80c9e5666ae
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-23 17:26:22 +0200

    Instructions for the Leap Year challenge

---
+++ 100 days of code: Python Bootcamp/Day 10/Challenge: Leap Year.py
@@ -0,0 +1,24 @@
+# Leap Year
+# ─────────
+# Write a function that returns True or False whether a given year is a leap year.
+
+# RULES:
+# - Divisible by 4 with no remainder → Leap year
+# - EXCEPT divisible by 100 with no remainder → Not a leap year
+# - UNLESS also divisible by 400 with no remainder → Leap year
+
+# EXAMPLES:
+# 2000 → True
+# 2100 → False
+# 1989 → False
+
+# WARNING: Your return must be a boolean (True/False), not a string.
+
+# ─────────────────────────────
+
+# def is_leap_year(year):
+#   # your code here
+
+# is_leap_year(2400)
+# is_leap_year(1989)
+
