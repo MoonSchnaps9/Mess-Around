09ca3fe97697e1146cfc7e4a2497c202e81570b9
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-25 18:49:06 +0200

    Step 3 done and instructions for the final part (Calculator)

--- 100 days of code: Python Bootcamp/Day 10/Project: Calculator.py
+++ 100 days of code: Python Bootcamp/Day 10/Project: Calculator.py
@@ -1,7 +1,7 @@
 #1: Write out the other 4 functions - subtract, mulptiply and divide
 
 def addition(n1, n2):
-    result = n1 - n2
+    result = n1 + n2
     return result
 
 def subtract(n1, n2):
@@ -26,4 +26,23 @@ maths_operation = {
 }
 
 #3: Use the dictionary operations to perform the calculations (Mulptiply 4 * 8 using the dictionary)
+
+print(maths_operation["*"](4, 8))
+
+# Calculator - Day 10
+# ────────────────────
+# Build a calculator that can perform addition, subtraction, multiplication and division.
+
+# Demo: https://appbrewery.github.io/python-day10-demo/
+
+# FUNCTIONALITY:
+# - Program asks the user to type the first number.
+# - Program asks the user to type a mathematical operator ("+", "-", "*" or "/").
+# - Program asks the user to type the second number.
+# - Program works out the result based on the chosen mathematical operator.
+# - Program asks if the user wants to continue working with the previous result.
+#   - If yes: program loops and uses the previous result as the first number, then repeats the calculation process.
+#   - If no: program asks the user for the first number again and wipes all memory of previous calculations.
+
+#----------------------------------------------------------------------------------------------------------------------------------
 
