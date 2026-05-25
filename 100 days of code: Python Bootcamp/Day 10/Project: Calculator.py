794afe81c162e675a590322da6c323e2553a71dc
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-25 20:00:59 +0200

    Solution - It works but it's a "Guillaume" version so I'll work on something cleaner :D

--- 100 days of code: Python Bootcamp/Day 10/Project: Calculator.py
+++ 100 days of code: Python Bootcamp/Day 10/Project: Calculator.py
@@ -1,3 +1,8 @@
+#Added this later during this project -> to clear the terminal
+from os import system
+
+
+
 #1: Write out the other 4 functions - subtract, mulptiply and divide
 
 def addition(n1, n2):
@@ -46,3 +51,59 @@ print(maths_operation["*"](4, 8))
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
+
+game = True
+
+while game:
+#ask user to choose the first number
+    user_first_number = int(input("What's your first galactic number?: "))
+
+#Display the list of operators
+    for operator in maths_operation:
+        print(operator)
+
+#aks user to choose the mathematical operator
+    user_operation_choice = input("Pick an operation: ")
+
+    #ask the user to choose the second number
+    user_second_number = int(input("What's your second galactic number?: "))
+
+    #Calculation
+    result = maths_operation[user_operation_choice](user_first_number, user_second_number)
+    print(f"{user_first_number} {user_operation_choice} {user_second_number} = {result}")
+
+    #ask user to choice if they want to continue working with the previous result
+    user_choice_continue_previous_number = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower()
+
+    #creating IF statement + While Loop
+    if user_choice_continue_previous_number == "n":
+        result = 0
+        system("clear")
+    elif user_choice_continue_previous_number == "y":
+        #Creating another while loop so the user can still work with his previous number until he's done so it goes back to the previous loop
+        game2 = True
+        while game2:
+            
+            user_first_number = result
+            
+            #Display the list of operators
+            for operator in maths_operation:
+                print(operator)
+
+            #aks user to choose the mathematical operator
+            user_operation_choice = input("Pick an operation: ")
+            
+            #ask the user to choose the second number
+            user_second_number = int(input("What's your second galactic number?: "))
+            
+            #Calculation
+            result = maths_operation[user_operation_choice](user_first_number, user_second_number)
+            print(f"{user_first_number} {user_operation_choice} {user_second_number} = {result}")
+            
+            #ask user to choice if they want to continue working with the previous result
+            user_choice_continue_previous_number = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower()
+
+            #If Statement + While loop
+            if  user_choice_continue_previous_number == "n":
+                result = 0
+                game2 = False
