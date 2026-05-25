a42942c497c3faa095f4e2957b574e661f04d453
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-25 18:21:53 +0200

    Step 2 done + instructions for step 3

--- 100 days of code: Python Bootcamp/Day 10/Project: Calculator.py
+++ 100 days of code: Python Bootcamp/Day 10/Project: Calculator.py
@@ -1,4 +1,8 @@
-#1: Write out the other 3 functions - subtract, mulptiplky and divide
+#1: Write out the other 4 functions - subtract, mulptiply and divide
+
+def addition(n1, n2):
+    result = n1 - n2
+    return result
 
 def subtract(n1, n2):
     result = n1 -n2
@@ -13,3 +17,13 @@ def divide(n1, n2):
     return result
 
 #2: Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+
+maths_operation = {
+    "+": addition,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+#3: Use the dictionary operations to perform the calculations (Mulptiply 4 * 8 using the dictionary)
+
