97ad1eff494ca4ec43a81f9379070c391179bdb7
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-29 23:22:42 +0200

    Solution to Ali's challenge (that was tough!)

--- Colleague's challenge/Ali's challenge.py
+++ Colleague's challenge/Ali's challenge.py
@@ -52,17 +52,35 @@ star_list = [" ", "*", " "]
 
 #---------------------
 
+# star_list_start_point = [" ", "*", " "]
+# star_list_ongoing_change = ["*"]
+# number_of_try = 2
+# # print(star_list_start_point)
+
+# for star in range(number_of_try):
+#     star_list_ongoing_change.insert(0, " ")
+#     star_list_ongoing_change.append(" ")
+#     print(star_list_ongoing_change)
+#     for first_space in range(1):
+#         star_list_ongoing_change[first_space] = "*"
+#         star_list_ongoing_change[len(star_list_ongoing_change) - 1] = "*"
+#         print(star_list_ongoing_change)
+        
+#---------------------
+
 star_list_start_point = [" ", "*", " "]
 star_list_ongoing_change = ["*"]
-
-# print(star_list_start_point)
+number_of_try = int(input("Hey Ali, can you please choose a habibi number?\n"))
 
 for star in range(1):
-    star_list_ongoing_change.insert(0, " ")
-    star_list_ongoing_change.append(" ")
-    print(star_list_ongoing_change)
-    for first_space in range(1):
-        star_list_ongoing_change[first_space] = "*"
-        star_list_ongoing_change[len(star_list_ongoing_change) - 1] = "*"
-        print(star_list_ongoing_change)
-
+    star_list_ongoing_change = ["*"]
+    for number in range(number_of_try):
+        star_list_ongoing_change.insert(0, " ")
+        star_list_ongoing_change.append(" ")
+    print(*star_list_ongoing_change)
+    for space in range(number_of_try):
+        star_position_left = star_list_ongoing_change.index("*")
+        star_list_ongoing_change[star_position_left - 1] = "*"
+        star_position_right = len(star_list_ongoing_change) - star_position_left
+        star_list_ongoing_change[star_position_right] = "*"
+        print(*star_list_ongoing_change)
