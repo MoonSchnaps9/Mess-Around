15bac487d9215b74893594dd4c987f8f36a3e783
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-05-30 17:14:19 +0200

    Creating the choice + BlackJack logo

--- 100 days of code: Python Bootcamp/Day 11 (Capstone Project)/BlackJack Project.py
+++ 100 days of code: Python Bootcamp/Day 11 (Capstone Project)/BlackJack Project.py
@@ -9,6 +9,13 @@
 # - The cards in the list have equal probability of being drawn.
 # - Cards are not removed from the deck as they are drawn.
 # - The computer is the dealer.
+from Blackjackicon import logo
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_choice = input("Do you want to play our BlackJack 'Saturn' edition? Type 'Ofc' for yes and 'ofc not' for no\n ").lower()
 
+if player_choice == "ofc not":
+    print("Ok... you've hurt my feelings :(")
+
+if player_choice == "ofc":
+    print(logo)
