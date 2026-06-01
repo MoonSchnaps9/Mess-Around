903d7193ce2a244dbbaa8f418032401a0a8fc046
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-01 23:44:23 +0200

    Defining Functions before building the games :)

--- 100 days of code: Python Bootcamp/Day 11 (Capstone Project)/BlackJack Project.py
+++ 100 days of code: Python Bootcamp/Day 11 (Capstone Project)/BlackJack Project.py
@@ -13,21 +13,38 @@ import random
 from Blackjackicon import logo
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_cards = []
+computer_cards = []
 total_count_player = 0
+total_count_computer = 0
 
-#Player start hand
+#Player start hand function
 def first_player_draw(player_cards):
     for card in range(2):
         player_cards.append(random.choice(cards))
     return player_cards
 
+#Computer start hand function
+def first_computer_card(computer_cards):
+    for i in range(2):
+        computer_cards.append(random.choice(cards))
+    return computer_cards
+    
+#Player total count
+def player_total_count(total_count_player, player_cards):
+    for score in player_cards:
+        total_count_player += score
+    return total_count_player
+
+#Computer total count
+def computer_total_count(total_count_computer, computer_cards):
+    for score in computer_cards:
+        total_count_computer += score
+    return total_count_computer
+
 print(first_player_draw(player_cards))
-
-
-# #Computer start hand
-# computer_card_start = []
-# for card in range(2):
-#     computer_card_start.append(random.choice(cards))
+print(first_computer_card(computer_cards))
+print(player_total_count(total_count_player, player_cards))
+print(computer_total_count(total_count_computer, computer_cards))
 
 # #Welcome Phrase
 # player_choice_start_game = input("Do you want to play our BlackJack 'Saturn' edition? Type 'Ofc' for yes and 'ofc not' for no\n ").lower()
@@ -42,7 +59,7 @@ print(first_player_draw(player_cards))
 #     game = True
 #     while game:
 #         print(logo)
-#         print(f"Your celestial cards are {player_card_start[0]}, {player_card_start[1]}. Your current score is {total_count_player}")
+#         print(f"Your celestial cards are {player_cards[0]}, {player_cards[1]}. Your current score is {total_count_player}")
 #         print(f"Titan's first card is {computer_card_start[0]}")
 #         Player_choice_during_game = input("type 'M' to get another card or type 'A' to pass").upper()
 #         if player_choice_during_game 
