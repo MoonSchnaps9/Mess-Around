ab2e979fce746212af8178a4de8e00f8fdf05187
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-02 23:12:52 +0200

    Overall structure defined, but many errors so I still need to work on this

--- 100 days of code: Python Bootcamp/Day 11 (Capstone Project)/BlackJack Project.py
+++ 100 days of code: Python Bootcamp/Day 11 (Capstone Project)/BlackJack Project.py
@@ -9,13 +9,19 @@
 # - The cards in the list have equal probability of being drawn.
 # - Cards are not removed from the deck as they are drawn.
 # - The computer is the dealer.
+from os import system
 import random
 from Blackjackicon import logo
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 player_cards = []
 computer_cards = []
+
 total_count_player = 0
 total_count_computer = 0
+
+player_above_21 = False
+computer_above_21 = False
 
 #Player start hand function
 def first_player_draw(player_cards):
@@ -28,39 +34,172 @@ def first_computer_card(computer_cards):
     for i in range(2):
         computer_cards.append(random.choice(cards))
     return computer_cards
+
+#Player pick another card
+def player_pick_another_card(player_cards):
+        player_cards.append(random.choice(cards))
+        return player_cards
+
+#Computer pick another card
+def computer_pick_another_card(computer_cards):
+    computer_cards.append(random.choice(cards))
+    return computer_cards
     
 #Player total count
 def player_total_count(total_count_player, player_cards):
-    for score in player_cards:
-        total_count_player += score
+    for u in player_cards:
+        total_count_player += u
     return total_count_player
 
 #Computer total count
 def computer_total_count(total_count_computer, computer_cards):
-    for score in computer_cards:
-        total_count_computer += score
+    for i in computer_cards:
+        total_count_computer += i
     return total_count_computer
 
-print(first_player_draw(player_cards))
-print(first_computer_card(computer_cards))
-print(player_total_count(total_count_player, player_cards))
-print(computer_total_count(total_count_computer, computer_cards))
+#Player above 21?
+def player_above_21_check(total_count_player, player_above_21):
+    if total_count_player > 21:
+        player_above_21 = True
+    return player_above_21
 
-# #Welcome Phrase
-# player_choice_start_game = input("Do you want to play our BlackJack 'Saturn' edition? Type 'Ofc' for yes and 'ofc not' for no\n ").lower()
+#Computer above 21?
+def computer_above_21_check(total_count_computer, computer_above_21):
+    if total_count_computer > 21:
+        computer_above_21 = True
+    return computer_above_21
 
-# #In case they say no
-# if player_choice_start_game == "ofc not":
-#     print("Ok... you've hurt my feelings :(")
+#Who wins?
+def winner_decision(total_count_computer, computer_above_21, total_count_player, player_above_21, player_cards, computer_cards):
+    if total_count_player > total_count_computer:
+        if player_above_21 == False:
+            if total_count_player == 21:
+                return f"Your final hand: {player_cards}, total score: {total_count_player}\n Computer final hand: {computer_cards}, total score: {total_count_computer}\n Saturn is impressed! Nice Blackjack :O"
+            else:
+                return f"Your final hand: {player_cards}, total score: {total_count_player}\n Computer final hand: {computer_cards}, total score: {total_count_computer}\n Congrats, you won! :D"
+    elif total_count_player < total_count_computer:
+        if computer_above_21 == False:
+            if total_count_computer == 21:
+                return f"Your final hand: {player_cards}, total score: {total_count_player}\n Computer final hand: {computer_cards}, total score: {total_count_computer}\n Titan has won you over with a Blackjack.. What a moon :'("
+            else:
+                return f"Your final hand: {player_cards}, total score: {total_count_player}\n Computer final hand: {computer_cards}, total score: {total_count_computer}\n Sorry, you lose! :("
+    elif total_count_player == total_count_computer:
+        return f"Your final hand: {player_cards}, total score: {total_count_player}\n Computer final hand: {computer_cards}, total score: {total_count_computer}\n WOW, that's a draw.. the tension is celestial >:("
 
-# #In case they say yes
-# if player_choice_start_game == "ofc":
+
+#Welcome Phrase
+player_choice_start_game = input("Do you want to play our BlackJack 'Saturn' edition? Type 'Ofc' for yes and 'ofc not' for no\n ").lower()
+
+#In case they say no
+if player_choice_start_game == "ofc not":
+    print("Ok... you've hurt my feelings :(")
+
+#In case they say yes
+if player_choice_start_game == "ofc":
     
-#     game = True
-#     while game:
-#         print(logo)
-#         print(f"Your celestial cards are {player_cards[0]}, {player_cards[1]}. Your current score is {total_count_player}")
-#         print(f"Titan's first card is {computer_card_start[0]}")
-#         Player_choice_during_game = input("type 'M' to get another card or type 'A' to pass").upper()
-#         if player_choice_during_game 
+    first_loop = True
+    while first_loop:
+        print(logo)
+
+        first_player_draw(player_cards)
+        first_computer_card(computer_cards)
+        total_count_player = player_total_count(total_count_player, player_cards)
+        total_count_computer = computer_total_count(total_count_computer, computer_cards)
+
+        player_above_21 = player_above_21_check(total_count_player, player_above_21)
+
+
+        if  player_above_21 == True:
+            print(f"Saturn has decided to not like you, since your celestial cards are{player_cards[0]}, {player_cards[1]}\nwhich makes your current score at {total_count_player}")
+            try_again_player = input("Would you like to do another game? 'Y' for yes and 'I AM SAD' for no\n").upper()
+            if try_again_player == "Y":
+                  system("clear")
+            elif try_again_player == "I AM SAD":
+                game = False
         
+        elif player_above_21 == False:
+            print(f"Your celestial cards are {player_cards[0]}, {player_cards[1]}. Your current score is {total_count_player}")
+            print(f"Titan's first card is {computer_cards[0]}")
+            
+            player_first_choice_game = input("type 'G' to get another card or type 'P' to pass").upper()
+            
+            if player_first_choice_game == 'G':
+                second_loop = True
+                while second_loop:
+                    player_pick_another_card(player_cards)
+                    player_above_21 = player_above_21_check(total_count_player, player_above_21)
+                    
+                    if  player_above_21 == True:
+                        
+                        print(f"Saturn has decided to not like you, since your celestial cards are{player_cards[0]}, {player_cards[1]}\nwhich makes your current score at {total_count_player}.. Sorry :(")
+                        try_again_player = input("Would you like to do another game? 'Y' for yes and 'I AM SAD' for no\n").upper()
+                        
+                        if try_again_player == "Y":
+                            system("clear")
+                            second_loop == False
+                        elif try_again_player == "I AM SAD":
+                            second_loop == False
+                            game = False
+                    
+                    elif player_above_21 == False:
+                        print(f"Your celestial cards are {player_cards[0]}, {player_cards[1]}. Your current score is {total_count_player}")
+                        print(f"Titan's first card is {computer_cards[0]}")
+
+                        player_second_choice = input("type 'G' to get another card or type 'P' to pass").upper()
+
+                        if player_second_choice == "P":
+                            if total_count_computer > 17:
+                                print(winner_decision(total_count_computer, computer_above_21, total_count_player, player_above_21, player_cards, computer_cards))
+                                try_again_player = input("Would you like to do another game? 'Y' for yes and 'I AM SAD' for no\n").upper()
+                            
+                            else:
+                                until_17_loop = True
+                                while until_17_loop:
+                                    computer_pick_another_card(computer_cards)
+                                    total_count_computer = computer_total_count(total_count_computer, computer_cards)
+
+                                    if total_count_computer > 17:
+                                        computer_above_21_check(total_count_computer, computer_above_21)
+                                        winner_decision(total_count_computer, computer_above_21, total_count_player, player_above_21, player_cards, computer_cards)
+                                        try_again_player = input("Would you like to do another game? 'Y' for yes and 'I AM SAD' for no\n").upper()
+                            
+                                        if try_again_player == "Y":
+                                            until_17_loop = False
+                                            second_loop == False
+                                            system("clear")
+                                        elif try_again_player == "I AM SAD":
+                                            until_17_loop = False
+                                            second_loop == False
+                                            game = False
+
+            elif player_first_choice_game == 'P':
+                until_17_loop = True
+                while until_17_loop:
+                    computer_pick_another_card(computer_cards)
+                    total_count_computer = computer_total_count(total_count_computer, computer_cards)
+
+                    if total_count_computer > 17:
+                        computer_above_21_check(total_count_computer, computer_above_21)
+                        print(winner_decision(total_count_computer, computer_above_21, total_count_player, player_above_21, player_cards, computer_cards))
+                        try_again_player = input("Would you like to do another game? 'Y' for yes and 'I AM SAD' for no\n").upper()
+                            
+                        if try_again_player == "Y":
+                            until_17_loop = False
+                            system("clear")
+                        elif try_again_player == "I AM SAD":
+                            until_17_loop = False
+                            game = False
+
+
+            
+
+                                
+
+
+
+
+
+
+
+
+
