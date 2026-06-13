59295f2b1856ebb99a3e7e16b756fe1c0804ba34
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-13 14:26:18 +0200

    Instructions for the challenge + logo of the game

---
+++ 100 days of code: Python Bootcamp/Day 12/Challenge: Number Guessing Game.py
@@ -0,0 +1,9 @@
+#Challenge: Number Guessing Game
+
+#User needs to guess a number between 1 and 100
+#2 levels of difficulty: Easy (10 attempts) and Hard (5 attempts)
+
+#----------------------------------------------------------------
+
+from art import logo
+print(logo)
