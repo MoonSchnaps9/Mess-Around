e4cb0f4a8b5c18787c814f7cca2468dc901c2d7a
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-13 17:28:21 +0200

    Solution

--- 100 days of code: Python Bootcamp/Day 13/debugging (Part 2).py
+++ 100 days of code: Python Bootcamp/Day 13/debugging (Part 2).py
@@ -1,4 +1,10 @@
 from random import randint
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 6)
+dice_num = randint(0, 5)
 print(dice_images[dice_num])
+
+#Randint was incorrect for 1-6 
+#1 -> Because the value of ❶ in the list is 0 so never gets printed
+#6 -> the maximum value in the list is 5 with ❻
+
+#So 1-6 becomes 0-5
