7dd6d5535e04c971c535c5191c603b7e74df8b45
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-13 17:19:22 +0200

    Solution

--- 100 days of code: Python Bootcamp/Day 13/Debugging (Part 1).py
+++ 100 days of code: Python Bootcamp/Day 13/Debugging (Part 1).py
@@ -1,11 +1,22 @@
-def my_function():
-    for i in range(1, 20):
-        if i == 20:
-            print("You got it")
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it")
 
-my_function()
+# my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
 # 2. When is the function meant to print "You got it"?
 # 3. What are your assumptions about the value of i?
+
+
+#-------
+#How to debug
+# -> either to add i + 1 or go to 21 and not 20 since i == 20 never happens in this configuration
+def my_function():
+    for i in range(1, 20):
+        if i + 1 == 20:
+            print("You got it")
+
+my_function()
