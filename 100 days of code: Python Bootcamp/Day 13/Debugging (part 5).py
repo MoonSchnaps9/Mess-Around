aca6a08be1c1b78bafb414db1f27e1265b9a3b41
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-17 20:26:25 +0200

    Solution

--- 100 days of code: Python Bootcamp/Day 13/Debugging (part 5).py
+++ 100 days of code: Python Bootcamp/Day 13/Debugging (part 5).py
@@ -16,12 +16,20 @@
 
 def is_leap(year):
     if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 4000 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
+        if year % 100 != 0:
+            result = True
+            return result
+        elif year % 400 == 0:
+            result = True
+            return result
+        elif year % 400 != 0:
+            result = False
+            return result
     else:
-        return False
+        result = False
+        return result
+
+    
+print(is_leap(2021))
+
+    
