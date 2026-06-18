828b4faac2da117fff7960fc7b859e86e71e5f19
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-06-18 22:37:03 +0200

    Instructions of the FInal Project (Beginner Path): HigherLowerGame

---
+++ 100 days of code: Python Bootcamp/Final Beginner Project: HigherLowerGame.py
@@ -0,0 +1,7 @@
+#Final Beginner Project
+#Build from scratch the HigherLowerGame
+#Teacher gave us a variable that contained a list of dictionaires (I came up with my own) + Logo
+
+#----------------------------------------------------------------------------------------------------------
+
+
