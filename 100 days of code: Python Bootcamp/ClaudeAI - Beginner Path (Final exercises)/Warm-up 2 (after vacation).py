4a287971ff92447d70783b666e19c4fff2a12e76
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-08-21 20:26:14 +0200

    Warm-up two completed :)

--- 100 days of code: Python Bootcamp/ClaudeAI - Beginner Path (Final exercises)/Warm-up 2 (after vacation).py
+++ 100 days of code: Python Bootcamp/ClaudeAI - Beginner Path (Final exercises)/Warm-up 2 (after vacation).py
@@ -20,4 +20,40 @@ def display_inventory(inventory):
     for item in inventory:
         print(f"You have {inventory[item]} {item}")
 
-        
+def add_item(inventory):
+    quantity, item = int(input("How many Pluto do you.. I mean, how many to add?\n").lower()), (input("name?\n").lower())
+    for_loop_check = False
+    for resources in inventory:
+        if resources == item:
+            inventory[resources] += quantity
+            for_loop_check = True
+
+    if for_loop_check == False:
+        inventory[item] = quantity
+
+    print(inventory)
+    return(inventory)
+
+def remove_item(inventory):
+    quantity, item = int(input("How many Pluto do you.. I mean, how many to remove?\n").lower()), (input("name?\n").lower())
+    for_loop_check_T = False
+    for resources in inventory:
+        if resources == item:
+            if inventory[resources] - quantity < 0:
+                print("So, basically you would go below 0.. and it's like creating a black hole.. and we don't want that so.. try again please? 😅")
+                for_loop_check_T = True
+            else:
+                inventory[resources] -= quantity
+                print(inventory)
+                for_loop_check_T = True
+
+    if for_loop_check_T == False:
+        print("You basically want to remove some stuff in your inventory that do not exist...\n" \
+        "I mean.. it's like removing air in space where there's no air... but hey, I like this clean freak spirit!")
+
+    return(inventory)
+
+
+display_inventory(inventory)
+add_item(inventory)
+remove_item(inventory)
