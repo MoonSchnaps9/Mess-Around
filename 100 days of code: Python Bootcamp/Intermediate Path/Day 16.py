6a8e47209958780ff9d5550cd78d74be013d8ad3
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-05 16:30:45 +0200

    Training - Creating Pokemon table with Name and Type using PrettyTable package - added 2 columns and played with Object Methods

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 16.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 16.py
@@ -1,19 +1,21 @@
-import turtle
+# import turtle
 
-franklin = turtle.Turtle()
+# franklin = turtle.Turtle()
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-franklin = Turtle()
-print(franklin)
-franklin.shape("turtle")
-franklin.color("cyan")
-franklin.forward(100)
-my_screen = Screen()
-my_screen.canvheight
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# franklin = Turtle()
+# print(franklin)
+# franklin.shape("turtle")
+# franklin.color("cyan")
+# franklin.forward(100)
+# my_screen = Screen()
+# my_screen.canvheight
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-# from prettytable import PrettyTable
-# table = PrettyTable()
-# print(table)
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table)
