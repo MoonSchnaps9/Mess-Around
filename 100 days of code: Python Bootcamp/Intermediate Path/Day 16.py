6f81456d4c54552bfac0983d7d5960950c1600ed
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-05 16:24:49 +0200

    Training Day 16: How to use OOP (Turtle example)

---
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 16.py
@@ -0,0 +1,19 @@
+import turtle
+
+franklin = turtle.Turtle()
+
+from turtle import Turtle, Screen
+
+franklin = Turtle()
+print(franklin)
+franklin.shape("turtle")
+franklin.color("cyan")
+franklin.forward(100)
+my_screen = Screen()
+my_screen.canvheight
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# print(table)
