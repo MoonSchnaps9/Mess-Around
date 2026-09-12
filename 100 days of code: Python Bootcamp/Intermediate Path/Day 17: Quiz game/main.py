1252f15a03180a57e7423844ea96252847380840
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-12 16:04:17 +0200

    2nd Challenge done

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/main.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/main.py
@@ -4,3 +4,11 @@ from data import question_data
 #Write a FOR loop to iterate over the question_data.
 #Create a Question object from each entry in question_data.
 #Append each Question object to the question_bank.
+
+question_bank = []
+
+for item in question_data:
+    question = Question(item["text"], item["answer"])
+    question_bank.append(question)
+
+print(question_bank[0].text)
