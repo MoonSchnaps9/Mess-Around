c9202d0216d7447e0c94a009387f3dbb12e31e57
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-12 15:34:19 +0200

    2nd challenge

---
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/main.py
@@ -0,0 +1,6 @@
+from question_model import Question
+from data import question_data
+
+#Write a FOR loop to iterate over the question_data.
+#Create a Question object from each entry in question_data.
+#Append each Question object to the question_bank.
