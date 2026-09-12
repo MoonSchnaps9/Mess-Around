f3e4d6519740d6839621e6a0add80e8acc7ff1fe
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-12 15:17:45 +0200

    First task done: Creating our Question class

---
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/question_model.py
@@ -0,0 +1,5 @@
+#create a question class with an __int__() method with two attributes: Text and answer attributes
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
