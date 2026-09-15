91932c387a9076ae3f5edc91f1eeb859478e8703
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-15 17:24:40 +0200

    Challenge done :)

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/quiz_brain.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/quiz_brain.py
@@ -3,3 +3,7 @@
 # Initialise the question_number to 0
 # Initialise the question_list to an input
 
+class QuizBrain:
+    def __init__(self, question_bank):
+        self.question_number = 0
+        self.question_list = question_bank
