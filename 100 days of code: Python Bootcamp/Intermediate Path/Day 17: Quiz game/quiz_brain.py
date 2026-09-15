832b3761bb27761715b4f7ff42960d39cfb75061
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-15 18:07:05 +0200

    Instructions for the next challenge (While loop)

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/quiz_brain.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/quiz_brain.py
@@ -1,3 +1,4 @@
+#1ST INSTRUCTIONS
 #Create a class called QuizBrain
 # Write an __init__() method
 # Initialise the question_number to 0
@@ -8,9 +9,15 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_bank
 
+#2ND INSTRUCTIONS
 #Retrieve the item at the current question_number from the question_list
 #use the input() function to show the user the question text and ask for the user's answer
     
     def next_question(self):
         user_answer = input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)?: ")
         self.question_number += 1
+
+#3RD INSTRUCTIONS
+#Create method called Still_has_question()
+#Return a bolean depending on the value of question_number
+#Use the while loop to show the next question until the end
