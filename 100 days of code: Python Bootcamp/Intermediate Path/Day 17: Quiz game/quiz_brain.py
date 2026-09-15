f4c98df58f6916c85dd39d1b3352dbf895f82571
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-15 17:26:37 +0200

    Next challenge: next_question method to build (Instructions)

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/quiz_brain.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 17: Quiz game/quiz_brain.py
@@ -7,3 +7,6 @@ class QuizBrain:
     def __init__(self, question_bank):
         self.question_number = 0
         self.question_list = question_bank
+
+#Retrieve the item at the current question_number from the question_list
+#use the input() function to show the user the question text and ask for the user's answer
