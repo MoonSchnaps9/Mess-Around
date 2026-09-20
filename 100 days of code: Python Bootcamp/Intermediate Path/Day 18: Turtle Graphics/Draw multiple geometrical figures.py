c3039e4488c28ed2dd02ed9505f46303e50327ac
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-20 17:02:09 +0200

    Challenge completed

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/Draw multiple geometrical figures.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/Draw multiple geometrical figures.py
@@ -1,5 +1,31 @@
 #Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon
 #The color changes once the form is drawn
+from turtle import Turtle, Screen
+
+vega = Turtle()
+
+colors = [
+    "red", 
+    "orange", 
+    "yellow", 
+    "green", 
+    "blue", 
+    "purple", 
+    "magenta", 
+    "cyan", 
+    "brown", 
+    "black"
+]
+
+for step in range(3, 11):
+    vega.color(colors[step - 3])
+    angle = 360 / step
+    for _ in range(step):
+        vega.forward(100)
+        vega.right(angle)
 
 
+
+useful = Screen()
+useful.exitonclick()
 
