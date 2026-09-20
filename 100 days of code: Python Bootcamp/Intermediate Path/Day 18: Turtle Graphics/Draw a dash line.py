6fc2eacbb186086b52b9c958c4d43418a723eebd
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-20 16:01:41 +0200

    Challenge completed

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/Draw a dash line.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/Draw a dash line.py
@@ -1,1 +1,11 @@
 #Draw a dash line with Turtle
+from turtle import Turtle
+
+vega = Turtle()
+
+for _ in range(50):
+    vega.pendown()
+    vega.forward(10)
+    vega.penup()
+    vega.forward(10)
+
