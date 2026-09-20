6ae7241edb689fc983aa2b203f055bd38a86e0d9
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-20 14:38:26 +0200

    Setting up the main file + some examples from Angela with Turtle Graphics before challenges

---
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+vega = Turtle()
+vega.shape("turtle")
+vega.color("CadetBlue")
+vega.forward(150)
+vega.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
