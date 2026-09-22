a54745f4a92ba0b48c63bbc38c2fe1778077ddcd
MoonSchnaps9 <guillaume.briez@outlook.com>
2026-09-22 18:09:22 +0200

    Challenge done (Spirograph)

--- 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/Draw a Spirograph.py
+++ 100 days of code: Python Bootcamp/Intermediate Path/Day 18: Turtle Graphics/Draw a Spirograph.py
@@ -1,2 +1,29 @@
 #Draw a spirograph
+from turtle import Turtle, Screen
+import random
 
+vega = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    colors = (r, g , b)
+    return colors
+
+
+vega.speed(0)
+
+for _ in range(36):
+    colors = random_color()
+    vega.color(colors)
+    vega.left(10)
+    vega.circle(100)
+
+
+
+
+
+screen.exitonclick()
